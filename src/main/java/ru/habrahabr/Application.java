b93224ddf59b24dab8bbccbc923d086ef194049b
Ruslan Molchanov <ruslanys@gmail.com>
2015-08-27 23:27:45 +0600

    description

--- src/main/java/ru/habrahabr/Application.java
+++ src/main/java/ru/habrahabr/Application.java
@@ -3,6 +3,7 @@ package ru.habrahabr;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Lazy;
@@ -14,13 +15,14 @@ public class Application extends AbstractJavaFxApplicationSupport {
     @Value("${ui.title:JavaFX приложение}")//
     private String windowTitle;
 
+    @Qualifier("mainView")
     @Autowired
     private ConfigurationControllers.View view;
 
     @Override
     public void start(Stage stage) throws Exception {
         stage.setTitle(windowTitle);
-        stage.setScene(new Scene(view.getParent()));
+        stage.setScene(new Scene(view.getView()));
         stage.setResizable(true);
         stage.centerOnScreen();
         stage.show();
